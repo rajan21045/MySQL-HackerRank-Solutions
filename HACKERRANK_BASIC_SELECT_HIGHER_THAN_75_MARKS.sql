57da0cf3de7d404708b5d80e3eea2044e16a4cbb
Rajan Poudel <raajan.works@gmail.com>
2026-04-25 08:01:04 +0545

    Add files via upload

---
+++ HACKERRANK_BASIC_SELECT_HIGHER_THAN_75_MARKS.sql
@@ -0,0 +1,19 @@
+/*
+Hackerrank Question Link: https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true
+
+Question Title:
+Higher Than 75 Marks
+It Is From Basic Selection
+
+Question Description:
+Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+*/
+
+/*
+Enter your query here.
+*/
+
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), ID;
