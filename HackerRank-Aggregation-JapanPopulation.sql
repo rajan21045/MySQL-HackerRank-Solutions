22568968a5ef1ebd6ba5104b91b388b216e40e51
Rajan Poudel <raajan.works@gmail.com>
2026-04-28 22:02:53 +0545

    Update HackerRank-Aggregation-JapanPopulation.sql

--- HackerRank-Aggregation-JapanPopulation.sql
+++ HackerRank-Aggregation-JapanPopulation.sql
@@ -1,5 +1,5 @@
 -- Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
-
+ 
 /*
     Enter your query here and follow these instructions:
     1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
