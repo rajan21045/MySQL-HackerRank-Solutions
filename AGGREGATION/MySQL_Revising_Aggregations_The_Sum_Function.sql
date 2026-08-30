0fea0c2b861bf137371317ec1fcf1d79965d37e6
Rajan Poudel <raajan.works@gmail.com>
2026-08-30 11:51:19 +0545

    Add files via upload

---
+++ AGGREGATION/MySQL_Revising_Aggregations_The_Sum_Function.sql
@@ -0,0 +1,16 @@
+-- Query the total population of all cities in CITY where District is California.
+SELECT SUM(population ) FROM CITY
+WHERE DISTRICT = 'California';
+
+-- Question Title: Revising Aggregations - The Sum Function
+-- Prepare > SQL >Aggregation > Revising Aggregations - The Sum Function
+-- Question Link: https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true
+-- Language: MySQL
+
+
+/*
+Author				PRASHANTB1984
+Difficulty			Easy
+Max Score			10
+Submitted By		839667
+*/
